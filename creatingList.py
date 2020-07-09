f4fc2639d23c5ebdc00bc9c9d01e563cf66658ba
vinuprasad25 <vinuprasadrx135@gmail.com>
2020-07-10 04:07:28 +0530

    first commit

---
+++ creatingList.py
@@ -0,0 +1,24 @@
+                                       #CREATING LIST IN PYTHON
+#Lists in Python can be created by just placing the sequence inside the square brackets[].
+#Unlike Sets, list doesn’t need a built-in function for creation of list.
+ #Unlike Sets, list may contain mutable elements.
+
+
+ #creating  List
+List1 = []
+print("Blank List:- ",List1)
+
+#Adding Elements in list
+List1= [10,20,30]
+print("The elements present in list are:-",List1)
+
+#Creating List of Strings and accesing by indexing
+List1=['Vinu','Prasad','Python','Developer']
+print(List1[1])
+print(List1[2])
+print(List1[3])
+
+#crerating Mutli-Dimensional List
+List1=[['Vinu','Prasad'],['Python','Developer']]
+print("the mutli-dimensinal list are :-",List1)
+print(List1[1])
